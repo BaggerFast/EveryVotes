156a582cbb472580e47f10ad18f3296b31a7e71e
Andrew Smirnov <smirnov@informatics.ru>
2020-12-22 17:27:00 +0300

    Add Create form to create_post

--- application/forms.py
+++ application/forms.py
@@ -1,1 +1,28 @@
 from django import forms
+
+
+class CreateVotingForm(forms.Form):
+    title = forms.CharField(
+        label='Название',
+        max_length=255,
+        min_length=4,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'exampleInputTitle',
+                'v-model': 'title'
+            }
+        )
+    )
+    description = forms.CharField(
+        label='Описание',
+        max_length=255,
+        widget=forms.Textarea(
+            attrs={
+                'rows': '4',
+                'class': 'form-control',
+                'id': 'exampleInputDescription',
+                'v-model': 'description'
+            }
+        )
+    )
--- application/modules/create_post.py
+++ application/modules/create_post.py
@@ -1,3 +1,6 @@
+from django.urls import reverse
+
+from application.forms import CreateVotingForm
 from application.views import *
 
 
@@ -5,17 +8,25 @@ def create_post_view(request):
     if request.method == 'POST':
         data = request.POST
         if request.user.is_authenticated:
-            post = Voting(
-                author=request.user,
-                title=data['title'],
-                description=data['description'],
-            )
-            post.save()
-            View.current.push_message({'alert': 'success', 'message': 'A new post has been created!'})
-            return redirect('/')
+            form = CreateVotingForm(data)
+            if form.is_valid():
+                post = Voting(
+                    author=request.user,
+                    title=form.data['title'],
+                    description=form.data['description'],
+                )
+                post.save()
+                View.current.push_message({'alert': 'success', 'message': 'A new post has been created!'})
+                View.current.context['form'] = form
+                return redirect(reverse('index'))
+            else:
+                View.current.context['form'] = form
+                View.current.push_message({'alert': 'danger', 'message': 'There is an error in the form!'})
+                return redirect(reverse('create_post'))
         else:
             View.current.push_message({'alert': 'danger', 'message': 'Oops, you are not logged in!'})
-            return redirect('/create_post')
+            return redirect(reverse('create_post'))
     elif request.method == 'GET':
         View.current = View(request, 'Create post', 'pages/create_post.html')
+        View.current.context['form'] = CreateVotingForm()
     return View.current.get_render_page()
