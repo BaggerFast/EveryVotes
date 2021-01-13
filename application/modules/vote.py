c0fbe817222587889b77a9678a4b855861d77dce
Andrew Smirnov <smirnov@informatics.ru>
2021-01-13 14:12:07 +0300

    Migrate from GET to POST method in vote form. Add tooltips for action icons. Reformat html code.

--- application/modules/vote.py
+++ application/modules/vote.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.shortcuts import get_object_or_404
 
 from application.models import Voting, VoteVariant, VoteFact
@@ -7,23 +6,39 @@ from application.views import *
 
 
 @login_required
-def vote_page(request, id):
+def vote_page(request, vote_id):
     View.current = View(request, 'Vote template', 'pages/vote.html')
-    vote = get_object_or_404(Voting, id=id)
+
+    vote = get_object_or_404(Voting, id=vote_id)
     variants = vote.votevariant_set.all()
-    vid = request.GET.get('vid', None)
-    if vid:
-        fact_variant = get_object_or_404(VoteVariant, id=vid)
-        fact_count = VoteFact.objects.filter(variant=fact_variant, author=request.user).count()
-        if fact_count > 0:
-            raise Http404  # Todo: tell user that he has already made a choice
-        if fact_variant.voting.id != id:
-            raise Http404  # Todo: tell user that wrong variant has passed
-        fact = VoteFact(variant=fact_variant, author=request.user)
-        fact.save()
     facts = VoteFact.objects.filter(variant__voting=vote, author=request.user)
-    View.current.context['vote'] = vote
-    View.current.context['variants'] = variants
-    View.current.context['facts'] = facts
+    facts_count = facts.count()
+
+    View.current.context.update({
+        'vote': vote,
+        'variants': variants,
+        'errors': [],
+        'facts': facts,
+    })
+
+    if request.method == 'POST':
+        variant_id = request.POST.get('action', None)
+
+        if variant_id:
+            fact_variant = get_object_or_404(VoteVariant, id=variant_id)
+
+            if fact_variant.voting.id != vote_id:
+                View.current.context['errors'].append('Нельзя голосовать за варианты другого голосования')
+                return View.current.get_render_page()
+
+            if facts_count > 0:
+                View.current.context['errors'].append('Вы уже проголосовали')
+                return View.current.get_render_page()
+
+            fact = VoteFact(variant=fact_variant, author=request.user)
+            fact.save()
+        else:
+            View.current.context['errors'].append('Не указаны детали голоса')
+            return View.current.get_render_page()
 
     return View.current.get_render_page()
