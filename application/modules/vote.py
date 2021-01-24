02bdc966ea02c451444a8c547dabe2eaf8c13bcd
Daniil Aleksandrov <riosha3@gmail.com>
2021-01-25 00:19:50 +0300

    add vote_finished

--- application/modules/vote.py
+++ application/modules/vote.py
@@ -15,13 +15,11 @@ class VotePage(View):
     def get(self, request, id):
         self.context['navbar'] = get_navbar(request)
         vote = get_object_or_404(Voting, id=id)
-        variants = vote.votevariant_set.all()
-        variant_id = request.GET.get('vid', None)
-        facts = VoteFact.objects.filter(variant__voting=vote, author=request.user)
-
-        self.context['facts'] = facts
+        self.context['cur_var'] = VoteFact.objects.filter(variant__voting=vote, author=request.user)
         self.context['vote'] = vote
-        self.context['variants'] = variants
+        self.context['variants'] = VoteVariant.objects.filter(voting=vote)
+
+        variant_id = request.GET.get('vid', None)
 
         if variant_id:
             fact_variant = get_object_or_404(VoteVariant, id=variant_id)
@@ -29,21 +27,16 @@ class VotePage(View):
 
             if fact_count > 0:
                 messages.error(request, 'User has already made a choice.', extra_tags='danger')
-                return redirect(reverse('vote', args=[id]))
             if fact_variant.voting.id != id:
                 messages.error(request, 'Wrong variant has passed.', extra_tags='danger')
-                return redirect(reverse('vote', args=[id]))
-
             fact = VoteFact(variant=fact_variant, author=request.user)
             fact.save()
 
-        if facts.exists():
-            fact_variant_count = VoteFact.objects.filter(
-                variant__voting=vote,
-                variant__description=facts[0].variant.description  # TODO: replace with efficient func
-            ).count()
-            self.context['variant_votes'] = fact_variant_count
-        fact_total_count = VoteFact.objects.filter(variant__voting=vote).count()
-        self.context['total_votes'] = fact_total_count
-
+        # if self.context['cur_var'].exists():
+        #     self.context['variant_votes'] = VoteFact.objects.filter(
+        #         variant__voting=vote,
+        #         variant__description=self.context['cur_var'][0].variant.description  # TODO: replace with efficient func
+        #     ).count()
+        # fact_total_count = VoteFact.objects.filter(variant__voting=vote).count()
+        # self.context['total_votes'] = fact_total_count
         return render(request, Page.vote, self.context)
