141b58f4277f90bdde5812aac3926f177c42e4d6
Vladimir Vartanyan <vova.vartanian@bk.ru>
2021-01-26 17:09:55 +0300

    Add progress bar

--- application/modules/votings_list.py
+++ application/modules/votings_list.py
@@ -15,5 +15,7 @@ class VoteListView(View):
         for i in range(len(item)):
             item[i].check_settings()
         self.context['votings'] = Voting.objects.filter(closed=False).order_by("-created_at")
+        for i in range(len(self.context['votings'])):
+            self.context['votings'][i].progress = int((self.context['votings'][i].finish_at - timezone.now()).seconds /
+                                                      (self.context['votings'][i].finish_at - self.context['votings'][i].publish_at).seconds)
         return render(request, Page.votings_list, self.context)
-
