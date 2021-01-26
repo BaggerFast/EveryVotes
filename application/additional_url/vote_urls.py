87b959b2374e37e102a4c72ede238e9d101aa18b
Andrew Smirnov <smirnov@informatics.ru>
2021-01-26 17:12:29 +0300

    Fix urls

--- application/additional_url/vote_urls.py
+++ application/additional_url/vote_urls.py
@@ -4,13 +4,12 @@ from application.modules import *
 from application.modules.create_vote import CreateVoteView
 
 urlpatterns = [
-path('<int:id>/', VotePage.as_view(), name='vote'),
+    path('<int:id>/', VotePage.as_view(), name='vote'),
     path('create/', CreateVoteView.as_view(), name='create_vote'),
     path('pub_list/', VoteListView.as_view(), name='voting_list'),
     path('pvt_list/', OwnVoteListView.as_view(), name='own_voting_list'),
     path('edit/<int:cur_id>/', CreateEdiVoteView.as_view(), name='edit_vote'),
     path('remove/<int:id>/', RemoveVotePage.as_view(), name='remove_vote'),
-    path('user/<int:id>/', UserList.as_view(), name='user_list')
-    path('user_votes/<int:author_id>/',     login_required(UserVotesList.as_view()), name='user_votes'),
-    path('user_list/<int:variant_id>/',      login_required(UsersList.as_view()),     name='user_list')
+    path('user_votes/<int:author_id>/', login_required(UserVotesList.as_view()), name='user_votes'),
+    path('user_list/<int:variant_id>/', login_required(UsersList.as_view()), name='user_list')
 ]
