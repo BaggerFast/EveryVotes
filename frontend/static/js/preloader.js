449566510fd54c1677e9ecf64c03e970d15d5f6f
thienlao17 <danil.klimenko.vlad@mail.ru>
2022-04-13 16:46:00 +0300

    UPDATE: change preloader

--- frontend/static/js/preloader.js
+++ frontend/static/js/preloader.js
@@ -1,7 +1,13 @@
-window.onload = function () {
-  document.body.classList.add('loaded_hiding');
-  window.setTimeout(function () {
-    document.body.classList.add('loaded');
-    document.body.classList.remove('loaded_hiding');
-  }, 500);
-}
+var preload = $("#main_preload"), 
+    w = $(window).width() / 2 - 30, 
+    h = $(window).height() / 2 - 30;
+preload.html('<img src="../img/preloader.gif" alt="load content."/>');
+preload.css({"top": h + 'px', "left": w + 'px'});
+$(window).load(function () {
+    setTimeout(function () {
+        preload.animate({opacity: "0"}, 50, function () {
+            preload.html("")
+        });
+        $("#wrapper").animate({opacity: "1"}, 50)
+    }, 250)
+});
