10d7afbe870179f22319dafc1788bd02b980b9a0
BaggerFast <riosha3@gmail.com>
2022-04-19 23:54:24 +0300

    UPDATE: add verbose_name to backend/main/models.py/ user - username

--- backend/main/forms.py
+++ backend/main/forms.py
@@ -3,15 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegistrationForm(UserCreationForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['maxlength'] = 15
-
     class Meta:
         model = get_user_model()
         fields = ('username', 'password1', 'password2',)
-        labels = {'username': 'Логин', }
-        help_texts = {
-            "username": 'Не более 15 символов. Только буквы, цифры и символы @/./+/-/_.',
-        }
--- backend/main/models.py
+++ backend/main/models.py
@@ -7,12 +7,8 @@ class User(AbstractUser):
         max_length=20,
         unique=True,
         help_text='Требуется не более 20 знаков. Также можно использовать буквы, цифры и @/./+/-/_',
+        verbose_name='Логин',
     )
-    # email = models.EmailField(
-    #     verbose_name='email address',
-    #     max_length=255,
-    #     unique=True,
-    # )
 
     def __str__(self):
         return self.username
